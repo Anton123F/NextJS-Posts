ed562f407b718acb8b8d67989f1ae5f29b611922
Anton Fiadotauu <anton_fiadotau@epam.com>
2025-04-09 15:11:40 +0300

    add next.js Image for optimixzation, also generate metadata

--- components/posts.js
+++ components/posts.js
@@ -4,12 +4,20 @@ import { formatDate } from '@/lib/format';
 import LikeButton from './like-icon';
 import {togglePostLikeStatus} from "@/actions/post";
 import {useOptimistic} from 'react';
+import Image from "next/image";
+
+function imageLoader(config) {
+  console.log(config)
+  const [urlStart, urlEnd] = config.src.split('upload/');
+  const transformation = `w_200,q_${config.quality}`
+  return `${urlStart}upload/${transformation}/${urlEnd}`
+}
 
 function Post({ post, action }) {
   return (
     <article className="post">
       <div className="post-image">
-        <img src={post.image} alt={post.title} />
+        <Image loader={imageLoader} width={200} height={120} src={post.image} alt={post.title} quality={80} />
       </div>
       <div className="post-content">
         <header>
